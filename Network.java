6bfbddfa66a4a064dbb691ebedb578485df55535
idan klar <idanklar@Idans-MacBook-Pro.local>
2025-01-06 18:42:11 +0200

    net test 2

--- Network.java
+++ Network.java
@@ -29,8 +29,12 @@ public class Network {
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
+        if (name == null) {
+            return null;
+        }
+        name = name.toLowerCase();
         for(int i = 0; i < userCount; i++) {
-            if (name.equals(users[i].getName())) {
+            if (name.equals(users[i].getName().toLowerCase())) {
                 return users[i];
             }
         }
@@ -59,7 +63,8 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        if (getUser(name1) == null || getUser(name2) == null) {
+        if (getUser(name1) == null || getUser(name2) == null || getUser(name1).follows(name2) || name1.equals(name2)
+        || name1 == null || name2 == null) {
             return false;
         }
         User user = getUser(name1);
