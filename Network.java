7b2de793d14546262f7a724306082fe2797df149
idan klar <idanklar@Idans-MacBook-Pro.local>
2025-01-08 15:18:19 +0200

    test net 4

--- Network.java
+++ Network.java
@@ -130,7 +130,7 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String ans = "Network: \n";
+       String ans = "Network:\n";
        for (int i = 0; i < userCount; i++) {
             ans += users[i].toString() + "\n";
        }
